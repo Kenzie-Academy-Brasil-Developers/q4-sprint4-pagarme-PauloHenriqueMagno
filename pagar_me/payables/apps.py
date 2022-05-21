604ce8c3868eb50e730d4c5bc9834d51390413bd
PauloHenriqueMagno <paulohm2309@gmail.com>
2022-05-21 14:16:51 -0300

    feat(payables): change name and make model and migration

--- pagar_me/payables/apps.py
+++ pagar_me/payables/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class PayableConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'payable'
+    name = 'payables'
---
+++ pagar_me/payables/models.py
@@ -0,0 +1,12 @@
+from uuid import uuid4
+from django.db import models
+
+class Payables(models.Model):
+  id = models.UUIDField(primary_key = True, default = uuid4, editable = False)
+  status = models.CharField(max_length = 50)
+  payment_date = models.DateField()
+  amount = models.FloatField()
+
+  transaction = models.ForeignKey('transactions.Transactions', on_delete = models.CASCADE)
+  fee = models.ForeignKey('fees.Fees', on_delete = models.CASCADE)
+  seller = models.ForeignKey('users.Users', on_delete = models.CASCADE)
