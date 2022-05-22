211713ac0d0f25f117afb3ef046d42a7319b766a
PauloHenriqueMagno <paulohm2309@gmail.com>
2022-05-22 16:38:41 -0300

    feat(users): add create user view

--- pagar_me/users/views.py
+++ pagar_me/users/views.py
@@ -1,3 +1,48 @@
-from django.shortcuts import render
+from sqlite3 import IntegrityError
 
-# Create your views here.
+from django.contrib.auth import authenticate
+
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.authtoken.models import Token
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.request import Request
+from rest_framework.views import APIView
+
+from users.permissions import UserPermissions
+from users.models import Users
+
+from users.serializers import UserSerializer
+
+class UserView(APIView):
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [UserPermissions]
+
+    def post(self, request: Request):
+        try:
+            serializer = UserSerializer(data = request.data)
+            serializer.is_valid(raise_exception = True)
+
+            isEmailTaken = Users.objects.filter(
+                email = serializer.validated_data["email"]
+            ).exists()
+
+            if isEmailTaken:
+                return Response({"message": "User already exists"}, 422)
+
+            newUser = Users.objects.create(**serializer.validated_data)
+
+            newUser.set_password(serializer.validated_data['password'])
+
+            if newUser.is_admin:
+                newUser.is_seller = False
+
+            newUser.save()
+
+            userSerialized = UserSerializer(newUser)
+
+            return Response( userSerialized.data, 201 )
+
+        except IntegrityError as error:
+            return Response(str(error), 400)
+
