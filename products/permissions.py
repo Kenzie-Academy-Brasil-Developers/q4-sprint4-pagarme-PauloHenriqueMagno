5b17875293973ee6a5aa0af1c137a6270b58bc3a
PauloHenriqueMagno <paulohm2309@gmail.com>
2022-05-22 23:49:47 -0300

    feat(products): permissions

---
+++ products/permissions.py
@@ -0,0 +1,13 @@
+from rest_framework.permissions import BasePermission
+from rest_framework.request import Request
+
+class IsSeller(BasePermission):
+  def has_permission(self, request: Request, _):
+    allowed_methods = ['POST', 'PATCH']
+
+    if request.method in allowed_methods and (
+      request.user.is_anonymous or not request.user.is_seller
+    ):
+      return False
+      
+    return True
