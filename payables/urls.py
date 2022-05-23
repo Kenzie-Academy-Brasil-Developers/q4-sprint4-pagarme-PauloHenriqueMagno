8fc8433db930ab0db2985dd0e2da55a5357a51cc
PauloHenriqueMagno <paulohm2309@gmail.com>
2022-05-22 23:53:19 -0300

    feat(payables): add views and urls

---
+++ payables/urls.py
@@ -0,0 +1,8 @@
+from rest_framework.urls import path
+
+
+from payables.views import PayablesView
+
+urlpatterns = [
+  path("payables/", PayablesView.as_view())
+]
--- payables/views.py
+++ payables/views.py
@@ -1,3 +1,36 @@
-from django.shortcuts import render
+from unittest import result
+from rest_framework.views import APIView
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.request import Request
+from rest_framework.response import Response
 
-# Create your views here.
+
+from payables.permissions import IsSeller
+from payables.models import Payable
+from payables.serializer import PayablesSerializer
+
+class PayablesView(APIView):
+  authentication_classes = [TokenAuthentication]
+  permission_classes = [IsSeller]
+
+  def get(self, request: Request):
+    payables = Payable.objects.filter(seller_id = request.user.id)
+
+    paid = sum([payable for payable in payables if payable.status == "paid"])
+      
+    payables = [payable for payable in payables if payable.status == "waiting_funds"]
+
+    serializer = PayablesSerializer(payables, many = True)
+
+    waiting_funds = 0
+
+    for payable in serializer.data:
+      payable_dict = dict(payable)
+      waiting_funds += -float(payable_dict['amount'])
+
+    result = {
+      "payable_amount_paid": paid,
+      "payable_amount_waiting_funds": float('{:.2f}'.format(waiting_funds)),
+    }
+
+    return Response(result, 200)
